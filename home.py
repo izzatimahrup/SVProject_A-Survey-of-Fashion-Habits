4ceae460004a45531b2ed657132df2c2a1649e89
izzatimahrup <n.izzatimahrup@gmail.com>
2026-01-12 03:17:19 +0800

    Update home.py

--- home.py
+++ home.py
@@ -1,6 +1,16 @@
 import streamlit as st
 import pandas as pd
 
+# ---------------------------------------------------------
+# LOAD DATA 
+# ---------------------------------------------------------
+@st.cache_data
+def load_data():
+    url = "https://raw.githubusercontent.com/izzatimahrup/SVProject_A-Survey-of-Fashion-Habits/refs/heads/main/Cleaned_FashionHabitGF.csv"
+    return pd.read_csv(url)
+
+df = load_data()
+
 # =========================================================
 # HOMEPAGE
 # =========================================================
