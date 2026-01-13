3f9c304f7a0cd3d26ebac19427c2dabe37f235b3
nuraina21 <s22a0058@siswa.umk.edu.my>
2026-01-14 01:57:54 +0800

    Update consumer_motivation.py

--- consumer_motivation.py
+++ consumer_motivation.py
@@ -120,9 +120,7 @@ fig_dumbbell.update_layout(
 st.plotly_chart(center_title(fig_dumbbell), use_container_width=True)
 
 
-
-with st.expander("📝 Detailed Interpretation: Section A"):
-    st.write(f"""
+    st.info(f"""
     * **Ranking:** Respondents consistently prioritize **{top_m}**, suggesting that functional and aesthetic brand value outweighs community-seeking behavior.
     * **Gender Dynamics:** Look at the length of the grey lines in the dumbbell plot. A **long line** indicates a significant difference in motivation between genders, while **overlapping dots** show shared interests.
     * **Strategic Application:** If 'Discounts & Contests' shows a large gap, brand campaigns for that specific motivation should be targeted toward the high-scoring gender for better ROI.
